69a83b5be9d705ccdaf67966a3a45cd8e7458bdb
Victoria Nyamai <61185176+Tori-Stark@users.noreply.github.com>
2021-08-15 21:55:37 +0300

    Add files via upload
    navigation bar, header and footer

---
+++ includes/auth_check.php
@@ -0,0 +1,8 @@
+<?php
+if(!isset($_SESSION['useremail'])){
+    header('Location: log_in.php');
+
+}
+
+
+?>
---
+++ includes/navbar2.php
@@ -0,0 +1,24 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #1A237E;">
+    <div class="container">
+        <h2><a class="navbar-brand" href="#">Makanikapp</a></h2>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class=" d-flex" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto  text-right">
+
+                <li class="nav-item">
+                    <a class="nav-link "  href="sign_up.php"><b>Sign Up</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link "  href="log_in.php"><b>Sign In</b></a>
+                </li>
+            </ul>
+
+
+
+        </div>
+    </div>
+</nav>
